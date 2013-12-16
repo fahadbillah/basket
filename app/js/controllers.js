5fad73e91d9adcce822344487c0ecb621f32cac1
fahadbillah <fahadbillah@yahoo.com>
2013-12-16 09:14:56 +0600

    first commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,35 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyBasket', ['$scope','product',function($scope,product) {
+  	$scope.defaultPrice = "0.00";
+  	$scope.disabled = true;
+    product.getProducts().then(function(p){
+  		$scope.productList = p;
+    });
 
+    $scope.vat = 0.2;
+
+    $scope.delete = function(id){
+    	$scope.productList.splice(id, 1);
+    };
+    
+    $scope.subTotalPrice = 0;
+
+    $scope.subTotal = function(){
+    	var tempSubTotal = 0;
+    	angular.forEach($scope.productList, function(item) {
+        tempSubTotal += item.qty * item.price;
+      });
+      $scope.subTotalPrice = tempSubTotal;
+    	return tempSubTotal;
+    };
+
+    $scope.totalVat = function(sub){
+    	return sub*$scope.vat;
+    };
+
+    $scope.total = function(sub){
+    	return sub+(sub*$scope.vat);
+    };
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,21 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', [])
+	.value('version', '0.1')
+  .factory('product',['$http','$q', function($http,$q){
+  	return {
+      name: 'product',
+      readonly: false,
+      getProducts: function() {
+        var deferred = $q.defer();                       
+		    var link = "products/list.json";
+		    $http.get(link).success(function(data) {
+		      deferred.resolve(data);
+		    }).error(function(){
+		      deferred.reject();
+		    });
+				return deferred.promise;
+      }
+    };
+  }]);
